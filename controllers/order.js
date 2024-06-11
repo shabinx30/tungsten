c782da7d6ce5d5ae37dd06b1d8ea2e8b7693b90f
shabeensharih <shabeensharih@gmail.com>
2024-06-11 15:02:57 +0530

    admin ui changes

--- controllers/order.js
+++ controllers/order.js
@@ -2,7 +2,8 @@ const Order = require('../models/orders')
 
 const orderlist = async (req,res)=>{
     try {
-        const orders = await Order.find({})
+        const orders = await Order.find({}).populate('orderedProducts.productId').exec();
+        console.log(orders.orderedProducts);
         res.render('orderList',{orders})
     } catch (error) {
         console.log(error.message);
